d6359675104505b4a984437a06c9200b4f01d8bd
Gonzalo Palma <gonzalopm3d@gmail.com>
2022-09-11 00:22:08 +0200

    first api weather

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark text-light mb-5">
+      <div className="container-fluid">
+        <h3 className="mx-auto">La Predicción</h3>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/Spinner.css";
+
+export const Spinner = () => {
+  return (
+    <div className="lds-facebook">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
